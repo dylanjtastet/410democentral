7bf682d49def17239caf07bb6325f5e7ea38cb7a
sam-nielsen <samnielsen65@gmail.com>
2020-04-30 18:34:49 -0400

    Help page additions

--- 410democentral-frontend/src/components/help/HelpPage.js
+++ 410democentral-frontend/src/components/help/HelpPage.js
@@ -43,9 +43,34 @@ const HelpPage = () => {
                     </div>
                 </div>
             </section>
+
             <section className="section">
                 <div className="container">
                     <h1 className="title is-3">Running Code</h1>
+                </div>  
+            </section>
+
+            <section className="section">
+                <div className="container">
+                    <h1 className="title is-3">Building a Course</h1>
+                    <p>
+                        The following section is meant to provide guidance to instructors who are building a course for their students.
+                        Note: As of right now, in order to become an instructor of a course, an administrator must manually add you as
+                        one. 
+                    </p>
+                </div>
+                <br></br>
+                <div className="container">
+                    <h2 className="title is-5">Navigating to Your Course</h2>
+                    <div className="columns">
+                        <div className="column is-two-thirds">
+                            <p>
+                                To view your course, select it from the course dropdown in the navbar. If you haven't added materials,
+                                the left-side menu should be empty. To add new categories and samples, elect "Admin View" in the upper
+                                right corner of the course view.
+                            </p>
+                        </div>
+                    </div>
                 </div>
                  
             </section>
